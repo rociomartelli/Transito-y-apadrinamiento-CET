84e191ec708b84e3180a9867ce48d98f0d9830cc
rociomartelli <rociomartelli41@gmail.com>
2024-08-27 21:33:38 -0300

    carrusel y cartas

--- js/java.js
+++ js/java.js
@@ -12,3 +12,28 @@ const categorias = document.querySelectorAll('.categoria');
                 categoria.style.fontSize = ''; // Restaurar el tamaño de letra original al quitar el mouse
             });
         });
+
+        // Cambiar color de fondo del botón al pasar el mouse
+document.querySelectorAll('.RefugioBoton').forEach(function(button) {
+    button.addEventListener('mouseover', function() {
+        button.style.backgroundColor = '#36240F'; // Color de fondo cuando se hace hover
+    });
+
+    button.addEventListener('mouseout', function() {
+        button.style.backgroundColor = ''; // Color de fondo original
+    });
+});
+
+// Restaurar color y hacer zoom en las imágenes al pasar el mouse
+document.querySelectorAll('.RefugiosImg').forEach(function(img) {
+    img.addEventListener('mouseover', function() {
+        img.style.filter = 'grayscale(0%)'; // Restaurar color al pasar el mouse
+        img.style.transform = 'scale(1.05)'; // Hacer un pequeño zoom en la imagen
+    });
+
+    img.addEventListener('mouseout', function() {
+        img.style.filter = 'grayscale(100%)'; // Aplicar escala de grises
+        img.style.transform = 'scale(1)'; // Restaurar tamaño original #5b483b
+    });
+});
+
